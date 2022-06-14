e0b32a0c6ff59d3014031b79a8bad67f133d12eb
Luna Köpke <luna@aciralith.com>
2022-06-14 09:14:24 +0200

    Added POST variables for login and register
    Post variables got added, no new features for now.

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ session_start();
 require_once('configuration.php');
 require_once("$function_folder/FileMgr.php");
 require_once("$function_folder/PageLoader.php");
+require_once("$function_folder/login.php");
 $GLOBALS["title"] = "GuTrödle";
 
 if(isset($_SERVER["HTTPS"]) && $debug == false){
@@ -20,12 +21,38 @@ $product_id = isset($_GET['pid']) ? $_GET['pid'] : null;
 $page = isset($_GET['p']) ? $_GET['p'] : null;
 
 /**
+ * Post variablen
+ */
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+$vorname = isset($_POST['vorname']) ? $_POST['vorname'] : null;
+$nachname = isset($_POST['nachname']) ? $_POST['nachname'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+
+/**
  * Informationen der Session.
  */
 $nutzer_id = isset($_SESSION['nutzerid']) ? $_SESSION['nutzerid'] : null;
 
+/*
+ * Initialisiere Template engine.
+ */
+$template = new PageLoader($template_folder, "error.php");
 
 // TODO: Get Userdata if exists (for login and userinfo page).
+
+if(isset($_POST['login']) && $_POST['login'] == 1){
+    // TODO: login
+    // TODO: get user from email and get salt
+    $salt = "test";
+    gen_hash($password, $salt);
+}
+
+if(isset($_POST['register']) && $_POST['register'] == 1){
+    // TODO: register
+    $salt = gen_salt();
+    $gen_hash($password, $salt);
+    // TODO: create user and nutzer entry in database
+}
 
 /**
  * Logout user
@@ -50,7 +77,6 @@ $GLOBALS["nav"] = $nav->getText();
 /**
  * Setze Page Template
  */
-$template = new PageLoader($template_folder, "error.php");
 if($template->setPage($page) == false){
     $GLOBALS["errno"] = 404;
     $GLOBALS["errmsg"] = "Couldn't find Webpage, probably the page is work in progress or got deleted.";
